1ee91c3359c772dc0b18a738a6bf73a9700deaaa
Harps1000 <Harpreet101gill@gmail.com>
2019-11-20 18:38:00 +0000

    Sort Form

--- src/Components/Homepage/ArticleList.js
+++ src/Components/Homepage/ArticleList.js
@@ -2,13 +2,18 @@ import React, { Component } from "react";
 import ArticleCard from "./ArticleCard";
 import * as api from "../api"
 import ErrorHandler from "../ErrorHandler"; 
+import SortForm from "./SortForm"
 
 class ArticlesList extends Component {
     state = { Articles: null, sortBy: "created_at", err: null}
     render(){
         return(
             <div>
-<h2>List of Articles</h2>
+<h3>Sort By:</h3>   
+        <SortForm
+          handleSubmit={this.handleSortSubmit}
+          default={this.state.sortBy}
+        />
 
 {this.state.err ? (
           <ErrorHandler err={this.state.err} />
@@ -20,6 +25,14 @@ class ArticlesList extends Component {
             </div>     
         )
     }
+
+    handleSortSubmit = (e, value) => {
+        e.preventDefault();
+        this.setState({ sortBy: value });
+      };
+
+
+
 fetchArticles = (sort, topic) => {
     return api.fetchAllArticles(sort, topic).then(({data})=>{
         this.setState({ Articles: data.articles, err: null})
@@ -30,6 +43,17 @@ componentDidMount() {
     this.fetchArticles(this.state.sortBy, this.props.topic).catch(err => {
       this.setState({ err: err });
     });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (
+      this.state.sortBy !== prevState.sortBy ||
+      this.props.topic !== prevProps.topic
+    ) {
+      this.fetchArticles(this.state.sortBy, this.props.topic).catch(err => {
+        this.setState({ err: err });
+      });
+    }
   }
 
 }
---
+++ src/Components/Homepage/SortForm.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react"; 
+
+class SortForm extends Component { 
+  state = { value: "created_at" }; 
+  render() { 
+    return (
+      <form
+        onSubmit={e => {
+          this.props.handleSubmit(e, this.state.value);
+        }}
+      >
+        <select value={this.state.value} onChange={this.handleChange}>
+          <option value="created_at">created_at</option>
+          <option value="title">title</option>
+          <option value="votes">votes</option>
+          <option value="comment_count">comment_count</option>
+        </select>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+  handleChange = event => {
+    this.setState({ value: event.target.value });
+  
+  };
+}
+
+export default SortForm;
