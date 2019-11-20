80a7d7a9fb5bb5ffe2eeb7fbe15bf2a95ec72dc7
Harps1000 <Harpreet101gill@gmail.com>
2019-11-20 21:34:57 +0000

    css

--- src/App.js
+++ src/App.js
@@ -4,19 +4,21 @@ import {Router} from "@reach/router"
 import ArticlesList from "./Components/Homepage/ArticleList";
 import Article from "./Components/SinglePage/SinglePage"
 import NavBar from "./Components/NavBar"
+import Header from "./Components/Homepage/Header"
 
 
 class App extends Component {
   state = { user: "jessjelly" };
 render(){
   return (
-    <div>
-    <h1>NC News</h1>
-    <NavBar/>
+    <div className="main-container" >
+    <Header className="main-head--mainitem"/>
+    <p className="user--mainitem">logged in as: jessjelly</p>
+    <NavBar className="NavBar--mainitem"/>
     <Router>
-          <ArticlesList path="/"  />
-          <ArticlesList path="/topics/:topic"  />
-          <Article path="/:article_id" user={this.state.user} />
+          <ArticlesList path="/"  className="home--mainitem" />
+          <ArticlesList path="/topics/:topic"  className="home--mainitem"  />
+          <Article path="/:article_id" user={this.state.user} className='article-page--mainitem' />
         </Router>
 
 
